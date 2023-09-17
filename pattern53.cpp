3f74302a44d5e36ebf9a04e980d30bdada6bcd8c
saifali9648 <sali964876@gmail.com>
2023-09-17 23:09:58 +0530

    pattern

--- pattern53.cpp
+++ pattern53.cpp
@@ -1,2 +1,14 @@
 #include<iostream>
 using namespace std;
+int main(){
+	for(int a=5;a>=1;a--){
+		for(int b=1;b<a;b++){
+			cout<<" ";
+		}
+		for(int c=a;c<=5;c++){
+			cout<<c;
+		}
+		cout<<endl;
+	}
+	return 0;
+}
