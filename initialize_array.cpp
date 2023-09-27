9a090ed3755e77d643f68051acf42cf600db9ab8
saifali9648 <sali964876@gmail.com>
2023-09-27 19:32:50 +0530

    intialize array

---
+++ initialize_array.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int arr[5]={0};
+	int brr[5];
+	int crr[5];
+	for (int i=0;i<5;i++)
+	{
+		brr[i]=1;
+	}
+	for(int i=0;i<5;i++)
+	{
+		cout<<arr[i];
+	}
+	cout<<endl;
+		for(int i=0;i<5;i++)
+	{
+		cout<<brr[i];
+	}
+	cout<<endl;
+		for(int i=0;i<5;i++)
+	{
+		cout<<crr[i];
+	}
+	return 0;
+	
+}
